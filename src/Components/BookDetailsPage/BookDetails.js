0237cbc7ef4eba20b28f6e608ae897e910787cfe
Abdur <mr1347509@gmail.com>
2024-11-18 19:20:08 +0530

    Online Library Project

--- src/Components/BookDetailsPage/BookDetails.js
+++ src/Components/BookDetailsPage/BookDetails.js
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 import { useFetchBookByID } from "../../CustomHooks/UseFetchBookByID";
 import MainLoader from "../../HelperComponents/Loader/MainLoader";
 import NavigateBackStack from "../../HelperComponents/NavigateBack/NavigateBackStack";
+import NotFound from "../NotFoundPage/NotFound";
 
 const BookDetails = () => {
     const Params = useParams();
@@ -27,48 +28,56 @@ const BookDetails = () => {
 
     return (
         <React.Fragment>
-            <MainHeader style={{ margin: "0 0 5px" }} />
-
-            <NavigateBackStack />
-
             {
-                isLoading ?
-                    <MainLoader />
+                Book && Book !== ""
+                    ?
+                    <>
+                        <MainHeader style={{ margin: "0 0 5px" }} />
+
+                        <NavigateBackStack />
+
+                        {
+                            isLoading ?
+                                <MainLoader />
+                                :
+                                <div className="book-details">
+
+                                    <section className="image-section">
+                                        <img src={Book?.image} alt="Book" />
+                                    </section>
+
+                                    <section className="details-section">
+                                        <h1>{Book?.title}</h1>
+
+                                        <div>
+                                            <h3>Author: <span>{Book?.author}</span></h3>
+                                            <h3>Downloads: <span>{Book?.downloads}</span></h3>
+                                            <h3>Genres:
+                                                {
+                                                    Book?.genres.length > 0
+                                                        ?
+                                                        Book.genres.map((genre, index) => (
+                                                            <span key={index}>{genre} {Book?.genres.length !== index + 1 ? "," : ""}</span>
+                                                        ))
+                                                        :
+                                                        <></>
+                                                }
+                                            </h3>
+                                            <h3>Pages: <span>{Book?.pages}</span></h3>
+                                            <h3>Price: <span>${Book?.price}</span></h3>
+                                            <h3>Ratings: <span>{Book?.ratings}</span></h3>
+                                            <h3>Reviews: <span>{Book?.reviews}</span></h3>
+                                            <h3>Language: <span>{Book?.language}</span></h3>
+                                            <h3>published: <span>{Book?.published}</span></h3>
+                                        </div>
+
+                                        <p>{Book?.description}</p>
+                                    </section>
+                                </div>
+                        }
+                    </>
                     :
-                    <div className="book-details">
-
-                        <section className="image-section">
-                            <img src={Book?.image} alt="Book" />
-                        </section>
-
-                        <section className="details-section">
-                            <h1>{Book?.title}</h1>
-
-                            <div>
-                                <h3>Author: <span>{Book?.author}</span></h3>
-                                <h3>Downloads: <span>{Book?.downloads}</span></h3>
-                                <h3>Genres:
-                                    {
-                                        Book?.genres.length > 0
-                                            ?
-                                            Book.genres.map((genre, index) => (
-                                                <span key={index}>{genre} {Book?.genres.length !== index + 1 ? "," : ""}</span>
-                                            ))
-                                            :
-                                            <></>
-                                    }
-                                </h3>
-                                <h3>Pages: <span>{Book?.pages}</span></h3>
-                                <h3>Price: <span>${Book?.price}</span></h3>
-                                <h3>Ratings: <span>{Book?.ratings}</span></h3>
-                                <h3>Reviews: <span>{Book?.reviews}</span></h3>
-                                <h3>Language: <span>{Book?.language}</span></h3>
-                                <h3>published: <span>{Book?.published}</span></h3>
-                            </div>
-
-                            <p>{Book?.description}</p>
-                        </section>
-                    </div>
+                    <NotFound />
             }
 
         </React.Fragment>
